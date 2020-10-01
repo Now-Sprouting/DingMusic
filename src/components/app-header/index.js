8a94e291e7dd62664ee6151e3a5e9b4da1625a26
Now_Sprouting <15542456716@163.com>
2020-10-01 16:43:51 +0800

    Header部分完成(yarn add antd AND @ant-design/icons)

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -3,14 +3,16 @@ import React, { memo } from 'react'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min'
 import { HeaderWrapper, HeaderLeft, HeaderRight } from './style'
 import { headerLinks } from '@/common/local-data'
+import { SearchOutlined } from '@ant-design/icons'
+import { Input } from "antd";
 
 export default memo(function DAppHeader() {
     const showSelectItem = (item, index) => {
         if (index < 3) {
             return (
-            <NavLink to={item.link} key={item.title} className="linkItem" exact>{item.title}
-                <i className="icon"></i>
-            </NavLink>)
+                <NavLink to={item.link} key={item.title} className="linkItem" exact>{item.title}
+                    <i className="icon"></i>
+                </NavLink>)
         } else {
             return (<a href={item.link} key={item.title} className="linkItem">{item.title}</a>)
         }
@@ -34,6 +36,9 @@ export default memo(function DAppHeader() {
 
                 </HeaderLeft>
                 <HeaderRight>
+                    <Input className="search" placeholder="音乐/视频/电台/用户" prefix={<SearchOutlined />} />
+                    <div className="center">创作者中心</div>
+                    <div className="login">登录</div>
                 </HeaderRight>
 
             </div>
